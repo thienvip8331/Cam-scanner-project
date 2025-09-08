53a6b6575d48d0cd632790063f19a0dae3586869
thienvip8331 <83415286+thienvip8331@users.noreply.github.com>
2025-09-08 21:05:12 +0700

    Add files via upload

---
+++ ocr_corrections.py
@@ -0,0 +1,22 @@
+# File này chỉ để quản lý bảng sửa lỗi OCR
+
+OCR_CORRECTIONS = {
+    # Ví dụ các lỗi thường gặp
+    "suctose": "sucrose",
+    "stra wberry": "strawberry",
+    "lavoring": "flavoring",
+    "natur el": "natural",
+    "col orin": "coloring",
+    "mo,": "mono",
+    "dela-carotenes": "beta-carotenes",
+    "vigt nam": "việt nam",
+    "tu hạnh nhân nghiền": "từ hạnh nhân nghiền",
+    "nude": "nước",
+    "dâu thực vat": "dầu thực vật",
+    "đậu phông nghiền": "đậu phộng nghiền",
+    "hat óc chó": "hạt óc chó",
+    "chiết | xuất": "chiết xuất",
+    "khẩm sóc khách hàng": "chăm sóc khách hàng",
+    "sita": "sữa"
+    
+}
